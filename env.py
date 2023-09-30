dff942babc06fea788e126be064a526b9cb3a136
Sallydw <90691775+Sallydw@users.noreply.github.com>
2023-09-29 21:40:19 -0700

    Add spotify credential template

--- env.py
+++ env.py
@@ -8,3 +8,7 @@ load_dotenv()  # Load variables from .env file
 @dataclass
 class Env:
     OPEN_AI_KEY: str = os.getenv('OPEN_AI_KEY')
+    SPOTIPY_CLIENT_ID: str = os.getenv('SPOTIPY_CLIENT_ID')
+    SPOTIPY_CLIENT_SECRET: str = os.getenv('SPOTIPY_CLIENT_SECRET')
+
+    
